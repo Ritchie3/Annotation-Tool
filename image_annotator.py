0ceae2705867c35caf58faea86580614a1d52788
EdgarCardenasDeLaHoz <35382761+EdgarCardenasDeLaHoz@users.noreply.github.com>
2021-01-06 13:05:53 +0100

    Add functions for displaying model

--- image_annotator.py
+++ image_annotator.py
@@ -10,8 +10,12 @@ from copy import deepcopy
 
 import cv2 
 
+###############################
+#  Annotator 
+###############################
 
 class Annotate():
+
     def __init__(self,filenames, exclude_labeled=True, run=True):
 
         if exclude_labeled:
@@ -28,13 +32,14 @@ class Annotate():
         self.fill = 0.25
         self.drawn = []
 
+        self.model = None
+        self.draw_prediction = False
+
         self.polygons =  [[] for i in range(len(filenames))]
         self.cur_polygons = []
 
         if run:
             self.open_window()
-
-    
 
     def open_window(self):
 
@@ -48,36 +53,73 @@ class Annotate():
         self.draw_image()
 
     def read_image(self,filename):
-        im = cv2.imread(filename) 
-        return im 
 
-    def draw_image( self ):
+        ## Add Switch
+        im = cv2.imread(filename) 
+
+        return im
+    
+    def load_data(self):
+
         filename = self.filenames[self.im_idx]
-        
+
         im = self.read_image(filename)
 
         if im is None:
             print( str(self.im_idx) + ": Not Found" )
             self.filenames.pop(self.im_idx)
             self.polygons.pop(self.im_idx)
-            self.draw_image()
+
+            im = self.load_data()
+        im = im *255
+        
+        if im.dtype==np.bool:        im = im*255
+        if im.ndim == 2:             im = im[...,None]
+        
+
+        im = im.astype(np.uint8)
+
+        return im 
+
+    def draw_image( self ):
+        
+        im = self.load_data()
 
         self.ax.clear()
-        self.ax.imshow(im, cmap=self.cmap)  
+        
+        if self.model is not None:
+            if self.draw_prediction:
 
-        instructions =  "Left: Last Image                    Right: Next Image \n" + \
-                        "Up: Iterate Index up                Down: Iterate Index Down \n" + \
-                        "Enter/Right Click: Next Object      Backspace: Remove Point\n" + \
-                        "Escape: Close Tool \n"
+                pred = self.model.segment(im)
 
+                im = np.mean(im,axis=2)           
+                im = im[...,None]*[1,1,1]          
 
-        fn_str = filename
+                lb_rgb = pred.transpose([1,2,0])
+                lb_rgb = lb_rgb * [1,0,0] 
+                im = im*(1-lb_rgb) + lb_rgb*255
+                #
+        if im.shape[-1]==1:          im = im*[1,1,1]
+        im = im.astype(np.uint8)
+        self.ax.imshow(im)      
+
+        instr = [["Left: Last Image","Right: Next Image" ],
+                        ["Up: Iterate Index up","Down: Iterate Index Down"],
+                        ["Backspace: Remove Point","Enter/Right Click: Next Object"],
+                        ["Escape: Close Tool","[i] : More information"]]
+
+        # Add a table at the bottom of the axes
+        table = plt.table(cellText= instr, loc='top')
+        table.set_fontsize(30)
+        table.scale(1,3)
+
+        fn_str = self.filenames[self.im_idx]
         fn_str = ("..."+fn_str[-20:] if len(fn_str) > 20 else fn_str)
 
-        self.ax.set_title( instructions , fontsize = 20  )
-        self.ax.set_xlabel(fn_str + "\n Label " + str(self.cur_idx),  fontsize = 20 )
+        #self.ax.set_title( instructions , fontsize = 20  )
+        self.ax.set_xlabel(fn_str + "\n Label " + str(self.cur_idx),  fontsize = 40 )
         self.draw_polygons()
-        
+    ##  UI   
     def onKey(self, event):
         if event.key == 'right':
             self.change_image(1)
@@ -121,6 +163,10 @@ class Annotate():
             self.cmap = "gray"
             self.draw_image( )
 
+        if event.key == "m":
+            self.draw_prediction = not self.draw_prediction
+            self.draw_image( )
+
         if event.key == "c":
             last_poly = self.polygons[self.im_idx-1]
             if len(last_poly[-1])>0:
@@ -149,14 +195,13 @@ class Annotate():
 
     def onScroll(self,event):
         self.change_image(int(event.step))
-
+    ## Image stuff 
     def change_image(self,step):
 
         self.polygons[self.im_idx] = self.cur_polygons
         self.im_idx = (self.im_idx+step) % len(self.filenames)
         self.cur_polygons = self.polygons[self.im_idx] 
         self.draw_image()  
-
 
     def submit_polygon(self):
         new_poly = {"idx":self.cur_idx, "pts":[]}
@@ -182,7 +227,8 @@ class Annotate():
                 self.drawn.extend( self.ax.fill(polygons[:,0],polygons[:,1], clrs[L_idx], alpha=fill_alpha) )
                 self.drawn.extend( self.ax.plot(polygons[:,0],polygons[:,1], "-o"+clrs[L_idx] ))
         self.ax.figure.canvas.draw_idle()
-
+    
+    ## Drawing 
     def save_label_images(self):
 
         filenames = self.filenames
@@ -195,12 +241,10 @@ class Annotate():
             if len(poly)==0:  
                 continue
 
-
             print("saving labels for "+ str(len(poly)) + " objects:" + fn) 
             im = self.read_image(fn)
             labeled = self.gen_index_image(im,poly)
             self.save_labeled( fn, labeled )
-
 
     def gen_index_image(self,im,poly):
 
@@ -221,27 +265,42 @@ class Annotate():
 
         return  label_im
 
-            
-    def save_labeled(self, fn, label_im):
+    def save_labeled(self,fn, label_im):    
 
-        fn_out = fn.replace('_input','_label')    
-        io.imsave(fn_out , label_im)
-
-
-
-
-
-
-
-
-
-
-
-
+        #Add switch if fn is tiff / H5
+        save_labeled(fn, label_im)
+        
+###########################################
+#     Helper Functions 
 ###########################################
 
-###########################################
+def save_labeled(fn, label_im):
+    fn_out = fn.replace('_input','_label')    
+    io.imsave(fn_out , label_im)
 
+def read_H5(fn):
+    dataset = "mask_data"
+    with h5py.File(fn, 'r') as fh:   
+        if dataset not in fh.keys(): 
+            return None
+        data = np.array(  fh[dataset][:]  )       
+    return data
+
+def add_dataset(fn, data, dataset="labels"):
+    with h5py.File(fn, 'r+') as fh:
+        if dataset in fh.keys():  
+            del fh[dataset]
+            fh[dataset] = data
+        else:   
+            fh.create_dataset(dataset, data=data , compression="lzf")
+
+def read_image(self,filename):
+    im = cv2.imread(filename) 
+    return im
+
+###########################################
+##          Video Frame Selecting 
+###########################################
 
 def select_video_frames(filename, out_dir="./training"):
     
@@ -265,8 +324,7 @@ def select_video_frames(filename, out_dir="./training"):
                      
     cap.release()
     cv2.destroyAllWindows()  # destroy all the opened windows
-    
-    
+      
 def select_dataset_frames(filename, data, out_dir="./training"):
      
     if not os.path.exists(out_dir):   os.makedirs(out_dir)
@@ -287,7 +345,6 @@ def select_dataset_frames(filename, data, out_dir="./training"):
                      
     cv2.destroyAllWindows()  # destroy all the opened windows
 
-    
 def save_frames(out_dir,fn,data,t):
 
     im = data[t]
@@ -306,7 +363,6 @@ def norm_uint8(im):
     
 def save_input_frame(out_dir,fn,t):
     read_frame(fn, n )
-    
     
 def read_frame(fn, n ):
     reader = cv2.VideoCapture(fn)
@@ -327,5 +383,6 @@ def check_keys(out_dir, fn, im, t):
             print(fn_out)           
     return quit
 
-        
+############################################
+# ##########################################      
         
